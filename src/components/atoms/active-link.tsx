22c6946e8c626d7ab5f2e5fbcc246ed12825ab51
axseinga <urbanowicz.uk@gmail.com>
2023-10-08 14:57:46 +0100

    fix: active link aria current

--- src/components/atoms/active-link.tsx
+++ src/components/atoms/active-link.tsx
@@ -29,7 +29,7 @@ export const ActiveLink = <T extends string>({
 		<Link
 			href={href}
 			className={clsx(className, isActive && activeClassName)}
-			aria-current={isActive && "page"}
+			{...(isActive && { "aria-current": "page" })}
 		>
 			{children}
 		</Link>
