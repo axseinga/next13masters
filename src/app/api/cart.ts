005fe82e6a820506bd95aacfc4fcd1a32d0eb79a
axseinga <urbanowicz.uk@gmail.com>
2023-10-10 16:19:27 +0100

    chore: module 4, add stripe payment

--- src/app/api/cart.ts
+++ src/app/api/cart.ts
@@ -1,4 +1,6 @@
 import { cookies } from "next/headers";
+import Stripe from "stripe";
+import { redirect } from "next/navigation";
 import { executeGraphql } from "./graphqlApi";
 import {
 	CartAddProductDocument,
@@ -67,4 +69,52 @@ export async function addToCart(orderId: string, productId: string) {
 		},
 		cache: "no-store"
 	});
+}
+
+export async function handlePaymentAction() {
+	"use server";
+
+	if (!process.env.STRIPE_SECRET_KEY) {
+		throw new Error("Missing STRIPE_SECRET_KEY");
+	}
+
+	const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+		apiVersion: "2023-08-16",
+		typescript: true,
+	});
+
+	const cart = await getCartFromCookies();
+
+	if (!cart) {
+		return;
+	}
+
+	// @todo better handling if there is no item
+
+	const checkoutSession = await stripe.checkout.sessions.create({
+		payment_method_types: ["card"],
+		metadata: {
+			cartId: cart.id,
+		},
+		line_items: cart.orderItems.map((item) => ({
+			price_data: {
+				currency: "gbp",
+				product_data: {
+					name: item.product?.name || "",
+				},
+				unit_amount: item.product?.price || 0,
+			},
+			quantity: item.quantity,
+		})),
+		mode: "payment",
+		success_url: "http://localhost:3000/cart/success?sessionId={CHECKOUT_SESSION_ID}",
+		cancel_url: "http://localhost:3000/cart/cancel",
+	});
+
+	if (!checkoutSession.url) {
+		throw new Error("Something went wrong");
+	}
+
+	cookies().set("cartId", "");
+	redirect(checkoutSession.url);
 }
--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
-import { getCartFromCookies } from "@/app/api/cart";
+import { getCartFromCookies, handlePaymentAction } from "@/app/api/cart";
 import { formatCurrency } from "@/utils/utils";
 import { IncrementProductQuantity } from "@/components/atoms/increment-item-quantity";
 import { RemoveButton } from "@/components/atoms/remove-button";
@@ -25,15 +25,20 @@ export default async function CartPage() {
 									{item.product?.price && formatCurrency(item.product?.price / 100)} x{" "}
 									<IncrementProductQuantity quantity={item.quantity} itemId={item.id} />
 								</span>{" "}
-								<RemoveButton itemId={item.id}/>
+								<RemoveButton itemId={item.id} />
 							</div>
 						</div>
 					</li>
 				))}
 			</ul>
-			<button className="rounded border border-slate-600 bg-transparent px-4 py-2 font-semibold text-slate-600 hover:border-transparent hover:bg-slate-600 hover:text-white">
-				Confirm order
-			</button>
+			<form action={handlePaymentAction}>
+				<button
+					type="submit"
+					className="rounded border border-slate-600 bg-transparent px-4 py-2 font-semibold text-slate-600 hover:border-transparent hover:bg-slate-600 hover:text-white"
+				>
+					Continue to payment
+				</button>
+			</form>
 		</div>
 	);
 }
